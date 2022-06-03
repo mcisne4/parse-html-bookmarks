8b175748c3c00db739f6f910821de03181ed4b97
mcisne4 <48368011+mcisne4@users.noreply.github.com>
2022-06-03 01:26:32 -0500

    Parse filenames

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log('ready!')
+import path from 'path'
+import {} from 'fs'
+
+/* dotenv */
+import { config } from 'dotenv'
+config()
+
+/* Get filenames from environmental variables */
+let bookmarkHTMLs: string[] = [
+    process.env.FILE1 || '',
+    process.env.FILE2 || '',
+    process.env.FILE3 || '',
+]
+
+/* Filter valid filenames entries */
+bookmarkHTMLs = bookmarkHTMLs
+    .filter(file => file.length > 0)
+    .map(file => path.resolve(file))
